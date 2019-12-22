714ea9befb26e6f1f819ee3c777444ca3e770a1c
adaptable-communicator-4011 <ahsanclouds@gmail.com>
2019-12-22 17:23:21 +0000

    Done.

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,4 +1,4 @@
-describe "Hello World" do 
+describe "Hello World" do
   it 'has a file hello_world.rb' do
     expect(File.exists?("hello_world.rb")).to be_truthy, "Did you create the file hello_world.rb? (If so, is it spelled exactly the same with the same capitalization?)"
   end
@@ -9,4 +9,7 @@ describe "Hello World" do
     expect($stdout).to receive(:puts).with("Hello World!")
     load './hello_world.rb'
   end
+end
+create new File.read("hello_world")
+put hello-world-ruby
 end
