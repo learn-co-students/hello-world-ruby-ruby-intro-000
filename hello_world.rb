995dbd5e53cd4d446bad03f65952105d4a50ee52
Avni Alp Mumcu <a.alp.mumcu@gmail.com>
2020-03-30 02:30:43 +0000

    Done.

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,5 @@
 puts "Hello World!"
 
-puts "This is my second program written with Ruby, date is 03/29/20200"
+puts "This is my second program written with Ruby, date is 03/29/2020"
 
 puts "and I like to add one thing about this date that I live in NYC and NYC has been declared as the epicenter of Coronavirus as if this week, meaning that I am trying to learn coding while trying to overcome all the stress. If anybody reading this just know that we will overcome this pandemic and the world will be a better place to live, it should be for our children!"
